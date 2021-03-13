16a75ddb64dd9c17edce9dbdfaa4b1e50389b7fc
SOVEREIGN <isimemeni@gmail.com>
2021-03-12 19:43:14 -0500

    added function

--- training2.js
+++ training2.js
@@ -48,3 +48,13 @@ function countSmileys(arr) {
   }
   return smilies
 }
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+function solution(str, ending){
+  // TODO: complete
+  let sub = str.substring(str.length - ending.length)
+  return sub === ending ? true : false
+
+  // return str.endsWith(ending);
+}
