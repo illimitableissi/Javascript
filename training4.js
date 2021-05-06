33c22e615448c465663dd5d127013c17f289e78d
SOVEREIGN <isimemeni@gmail.com>
2021-05-05 22:09:39 -0400

    Added function

--- training4.js
+++ training4.js
@@ -61,4 +61,16 @@ function squareSum(numbers){
     return str.match(regex) == null ? 0 : str.match(regex).length
   }
 
-//review
+  // Create a function that transforms any positive number to a string representing the number in words. The function should work for all numbers between 0 and 999999.
+
+
+var num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+var tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+function number2words(n){
+  if (n < 20) return num[n];
+  var digit = n%10;
+  if (n < 100) return tens[Math.floor(n/10)-2] + (digit ? "-"+num[digit] : "");
+  if (n < 1000) return num[Math.floor(n/100)] +" hundred" + (n%100 == 0 ? "" : " " + number2words(n%100));
+  return number2words(Math.floor(n/1000)) + " thousand" + (n%1000 != 0 ? " " +number2words(n%1000) : "");
+}
